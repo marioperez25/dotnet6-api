510a5fd6c90ebbcc5ec9bd12bbed31d17f14b44f
Mario Perez <mariofph@gmail.com>
2022-07-06 10:44:56 -0600

    API Creation Tutorial:
    Skipped "Call the web API with JavaScript", now moving forward with "Add authentication support to a web API"
    
    Reinstated Swagger

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -1,8 +1,19 @@
 namespace TodoApi.Models
 {
     public class TodoItem {
-        public long Id { get; set;}
-        public string? Name { get; set;}
+        public long Id {get;set;}
+        public string? Name {get; set;}
         public bool IsComplete {get;set;}
+        public string? Secret {get;set;}
+    }
+}
+
+namespace TodoApi.Models
+{
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddDbContext<TodoContext>(opt =>
     opt.UseInMemoryDatabase("TodoList"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -18,8 +18,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    // app.UseSwagger();
-    // app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
